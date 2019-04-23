430c8dc02f9866b1f5e6179d2e765429d0e6841b
Robert <roberinho08@gmail.com>
2019-04-22 20:48:38 -0700

    Add Jumbotron and Cards that display CryptoNews

--- crypto/views.py
+++ crypto/views.py
@@ -4,6 +4,11 @@ def home(request):
 	import requests
 	import json
 	API_KEY = "c372b5f5d7d17814dc34b99ccf63bc07a31334c3dea7a518ac04d663eb1956de" # this contains our API Key
-	api_request = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR" +"&api_key=" +API_KEY)
+
+	# FETCH CRYPTO PRICE DATA
+	api_request = requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD,EUR" +"&api_key=" +API_KEY)
+	api = json.loads(api_request.content) # read our json
+	# FETCH CRYPTO NEWS
+	api_request = requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN" +"&api_key=" +API_KEY)
 	api = json.loads(api_request.content) # read our json
 	return render(request,'home.html', {'api' : api})
