c26f90f036234c3747713b65118106bafccf73ae
Robert <roberinho08@gmail.com>
2019-04-22 20:17:47 -0700

    Incorporate CryptoCompare API into application

--- crypto/views.py
+++ crypto/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request,'home.html', {})
+	import requests
+	import json
+	API_KEY = "c372b5f5d7d17814dc34b99ccf63bc07a31334c3dea7a518ac04d663eb1956de" # this contains our API Key
+	api_request = requests.get("https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR" +"&api_key=" +API_KEY)
+	api = json.loads(api_request.content) # read our json
+	return render(request,'home.html', {'api' : api})
